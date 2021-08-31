1dcc456f5d3d22b067522ac1b3610b3097e484a1
Junda HUANG <1155160619@link.cuhk.edu.hk>
2021-08-31 20:49:29 +0800

    test first commit

---
+++ InductRead/InductRead.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		InductRead.ino
+ Created:	8/31/2021 8:31:05 PM
+ Author:	jdhuang
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
